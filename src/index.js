cefd5d212f7cedaaf55eb9c59ee3d444d6ce3fff
Ricardo Tassio <ricardotassio@gmail.com>
2019-06-25 20:04:12 -0300

    Add .gitignore

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const cors = require('cors')
+
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+mongoose.connect('mongodb+srv://semana:semana@cluster0-20jl0.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+})
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, *");
+    req.io = io
+    next();
+})
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads','resized')))
+app.use(require('./routes'))
+app.use(cors())
+server.listen(3333)
